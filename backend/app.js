552688af59ed4f3f98df55a55e38bb9079d28236
Kevin Prunty <kevin.prunty95@gmail.com>
2020-08-31 19:35:56 -0400

    Added express and server foundation

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+      id: '001',
+      title: 'First server-side post',
+      content: 'This is coming from the server!'
+    },
+    {
+      id: '002',
+      title: 'Second server-side post',
+      content: 'This is coming from the server!!'
+    }
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
+
+});
+
+
+
+
+module.exports = app;
