3b48c2d00196e71eb66c0e8690bcf6d33a765904
Kevin Prunty <kevin.prunty95@gmail.com>
2020-09-01 11:44:40 -0400

    Added mongodb functionality

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,79 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
 
 
+mongoose.connect("mongodb+srv://admin:R4aD7dEGNEyZM8Ij@cluster0.pnqxp.mongodb.net/mean-course?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to database!');
+})
+.catch(()=> {
+  console.log('Connection failed!');
+});
 
-app.use('/api/posts',(req, res, next) => {
-  const posts = [
-    {
-      id: '001',
-      title: 'First server-side post',
-      content: 'This is coming from the server!'
-    },
-    {
-      id: '002',
-      title: 'Second server-side post',
-      content: 'This is coming from the server!!'
-    }
-  ];
+app.use(bodyParser.json());
 
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, OPTIONS")
+  next();
+});
+
+app.post("/api/posts", (req, res, next)=>{
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: result._id
+    });
   });
 
 });
 
+app.get('/api/posts',(req, res, next) => {
+  // const posts = [
+  //   {
+  //     id: '001',
+  //     title: 'First server-side post',
+  //     content: 'This is coming from the server!'
+  //   },
+  //   {
+  //     id: '002',
+  //     title: 'Second server-side post',
+  //     content: 'This is coming from the server!!'
+  //   }
+  // ];
+
+  Post.find()
+    .then(documents => {
+      console.log(documents);
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    })
+    .catch();
+});
+
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  // console.log(req.params.id);
+
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json( {message: "Post deleted!" });
+  });
+
+});
 
 
 
