87182b5545165a3b6e56fa050aa7bc75a66e5e1f
Kevin Prunty <kevin.prunty95@gmail.com>
2020-09-08 18:33:29 -0400

    Added route guarding for create/edit posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { PostCreateComponent } from './posts/post-create/post-create.component'
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 
+import { AuthGuard } from "./auth/auth.guard";
+
 const routes: Routes = [
   { path: '', component: PostListComponent},
-  { path: 'create', component: PostCreateComponent},
-  { path: 'edit/:postId', component: PostCreateComponent},
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard]},
+  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent}
 ];
@@ -21,6 +23,7 @@ const routes: Routes = [
   ],
   exports: [
     RouterModule
-  ]
+  ],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {}
